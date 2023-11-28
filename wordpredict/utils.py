5f69b33545b661d12a95953abbc269c8b4c63fdb
2ndr9 <rk17tnd@gmail.com>
2023-11-28 20:47:27 +0900

    Refactor TrieNode class and update WordPredict class
    単語推薦のための正当な組み合わせの保存形式をtrie nodeにした

--- wordpredict/utils.py
+++ wordpredict/utils.py
@@ -1,79 +1,96 @@
 class TrieNode:
-    def __init__(self) -> None:
-        self.children: dict[str, "TrieNode"] = {}
+    # Class variable for unique identifier
+    _identifier_counter = 0
 
-        # only leaf nodes have this attribute
-        self.freq_and_word: tuple[float, str] | None = None
+    def __init__(self):
+        self.identifier = TrieNode._identifier_counter
+        TrieNode._identifier_counter += 1
 
-    def get_all_leaf_nodes(self) -> list["TrieNode"]:
-        leaf_nodes = [self] if self.freq_and_word else []
+        self.children = {}
+        self.parent = None
+        self.freq_and_word = None
+
+    def __eq__(self, other):
+        if isinstance(other, TrieNode):
+            return self.identifier == other.identifier
+        return False
+
+    def __hash__(self):
+        return hash(self.identifier)
+
+    def get_freq_and_word_of_all_leaf_nodes(self):
+        result = []
+
+        if self.freq_and_word is not None:
+            result.append(self.freq_and_word)
 
         for child in self.children.values():
-            leaf_nodes.extend(child.get_all_leaf_nodes())
+            result.extend(child.get_freq_and_word_of_all_leaf_nodes())
 
-        return leaf_nodes
+        return result
 
 
-def build_trie(corpus_words: list[str], corpus_freq: list[int]) -> TrieNode:
+def build_trie(corpus_words, corpus_freq):
     root = TrieNode()
+    root.parent = None
     word_freq = dict(zip(corpus_words, corpus_freq))
 
     for word, freq in word_freq.items():
         node = root
         for i, char in enumerate(word):
-            node = node.children.setdefault(char, TrieNode())
+            if char not in node.children:
+                node.children[char] = TrieNode()
+                node.children[char].parent = node
+            node = node.children[char]
 
-            if i == len(word) - 1:  # leaf node
+            if i == len(word) - 1:
                 node.freq_and_word = (freq, word)
 
     return root
 
 
-def find_node_for_prefix(root: TrieNode, prefix: str) -> TrieNode | None:
-    node = root
-    for char in prefix:
-        if char not in node.children:
-            return None
-        node = node.children[char]
-    return node
+def update_valid_nodes(old_valid_nodes, new_char_list, root):
+    new_valid_nodes = []
+
+    if not old_valid_nodes:
+        for new_char in new_char_list:
+            if new_char in root.children:
+                new_valid_nodes.append(root.children[new_char])
+    else:
+        for valid_node in old_valid_nodes:
+            for new_char in new_char_list:
+                if new_char in valid_node.children:
+                    new_valid_nodes.append(valid_node.children[new_char])
+
+    return new_valid_nodes
 
 
-def update_valid_prefixes(
-    old_valid_prefixes: list[str], new_char_list: list[str], root: TrieNode
-):
-    if len(old_valid_prefixes) == 0:
-        return [char for char in new_char_list if char in root.children]
+def get_autocomplete_candidates(valid_nodes, max_candidates, alpha):
+    if not valid_nodes:
+        return []
 
-    new_valid_prefixes = [
-        valid_prefix + new_char
-        for valid_prefix in old_valid_prefixes
-        for new_char in new_char_list
-        if new_char in find_node_for_prefix(root, valid_prefix).children
-    ]
-    return new_valid_prefixes
+    freq_and_word_list = []
+    for node in valid_nodes:
+        freq_and_word_list.extend(node.get_freq_and_word_of_all_leaf_nodes())
 
-
-def get_autocomplete_candidates(
-    root: TrieNode, valid_prefixes: list[str], max_candidates: int, alpha: float
-) -> list[str]:
-    list_of_freq_and_word_tuple = [
-        leaf.freq_and_word
-        for prefix in valid_prefixes
-        for leaf in find_node_for_prefix(root, prefix).get_all_leaf_nodes()
-    ]
-
-    user_input_len = len(valid_prefixes[0])
-
+    user_input_len = calc_node_depth(valid_nodes[0])
     alpha_applied = [
         (apply_alpha_penalty(freq, len(word), user_input_len, alpha), word)
-        for freq, word in list_of_freq_and_word_tuple
+        for freq, word in freq_and_word_list
     ]
 
     alpha_applied.sort(key=lambda x: x[0], reverse=True)
-    return [item[1] for item in alpha_applied[:max_candidates]]
+    return [word for _, word in alpha_applied[:max_candidates]]
 
 
-def apply_alpha_penalty(
-    freq: float, target_word_len: int, user_input_len: int, alpha: float
-) -> float:
-    return freq * (alpha ** (target_word_len - user_input_len))
+def apply_alpha_penalty(freq, target_word_len, user_input_len, alpha):
+    return freq * alpha ** (target_word_len - user_input_len)
+
+
+def calc_node_depth(node):
+    count = 0
+    current_node = node
+    while current_node.parent is not None:
+        count += 1
+        current_node = current_node.parent
+    return count
--- wordpredict/wordpredict.py
+++ wordpredict/wordpredict.py
@@ -1,43 +1,39 @@
 from wordpredict.utils import (
     build_trie,
     get_autocomplete_candidates,
-    update_valid_prefixes,
+    update_valid_nodes,
 )
 
 
 class WordPredict:
-    def __init__(self, corpus_words: list[str], corpus_freq: list[int], alpha=0.62):
-        self.valid_prefixes = []
+    def __init__(self, corpus_words, corpus_freq, alpha=0.62):
         self.root = build_trie(corpus_words, corpus_freq)
-
         self.alpha = alpha
+        self.valid_nodes = []
 
-    def update(self, new_char_list: list[str], max_candidates=6):
-        if len(new_char_list) > 0:
-            self.valid_prefixes = update_valid_prefixes(
-                self.valid_prefixes,
-                new_char_list,
-                self.root,
+    def update(self, new_char_list, max_candidates=6):
+        if new_char_list:
+            self.valid_nodes = update_valid_nodes(
+                self.valid_nodes, new_char_list, self.root
             )
-
-        return get_autocomplete_candidates(
-            self.root,
-            self.valid_prefixes,
-            max_candidates,
-            self.alpha,
-        )
+        return get_autocomplete_candidates(self.valid_nodes, max_candidates, self.alpha)
 
     def reset(self):
-        """Resets the list of valid prefixes."""
-        self.valid_prefixes = []
+        self.valid_nodes = []
 
     def get_current_candidates(self, max_candidates=6):
-        """
-        Returns the current candidates without updating.
-        """
-        return get_autocomplete_candidates(
-            self.root,
-            self.valid_prefixes,
-            max_candidates,
-            self.alpha,
-        )
+        return get_autocomplete_candidates(self.valid_nodes, max_candidates, self.alpha)
+
+    def undo(self, max_candidates=6):
+        if not self.valid_nodes:
+            return []
+
+        if self.valid_nodes[0].parent.identifier == self.root.identifier:
+            self.valid_nodes = []
+            return []
+        else:
+            unique_parents = {node.parent for node in self.valid_nodes}
+            self.valid_nodes = list(unique_parents)
+            return get_autocomplete_candidates(
+                self.valid_nodes, max_candidates, self.alpha
+            )
