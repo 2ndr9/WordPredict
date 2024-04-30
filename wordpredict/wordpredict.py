9e43c3d839f911a3d6a467329ef729a0286f83ec
2ndr9 <53454871+2ndr9@users.noreply.github.com>
2024-04-30 14:46:19 +0900

    update undo func

--- wordpredict/wordpredict.py
+++ wordpredict/wordpredict.py
@@ -9,31 +9,49 @@ class WordPredict:
     def __init__(self, corpus_words, corpus_freq, alpha=0.62):
         self.root = build_trie(corpus_words, corpus_freq)
         self.alpha = alpha
-        self.valid_nodes = []
+        self.valid_nodes_for_undo = []  # 2次元配列
 
     def update(self, new_char_list, max_candidates=6):
         if new_char_list:
-            self.valid_nodes = update_valid_nodes(
-                self.valid_nodes, new_char_list, self.root
+            self.valid_nodes_for_undo.append(
+                update_valid_nodes(
+                    (
+                        self.valid_nodes_for_undo[-1]
+                        if len(self.valid_nodes_for_undo) >= 1
+                        else []
+                    ),
+                    new_char_list,
+                    self.root,
+                )
             )
-        return get_autocomplete_candidates(self.valid_nodes, max_candidates, self.alpha)
+
+        return get_autocomplete_candidates(
+            self.valid_nodes_for_undo[-1], max_candidates, self.alpha
+        )
+
+    def next_word(self):
+        self.valid_nodes_for_undo.append([])
 
     def reset(self):
-        self.valid_nodes = []
+        self.valid_nodes_for_undo = []
 
     def get_current_candidates(self, max_candidates=6):
-        return get_autocomplete_candidates(self.valid_nodes, max_candidates, self.alpha)
+        return get_autocomplete_candidates(
+            self.valid_nodes_for_undo[-1], max_candidates, self.alpha
+        )
 
     def undo(self, max_candidates=6):
-        if not self.valid_nodes:
+        if len(self.valid_nodes_for_undo) == 0:
             return []
 
-        if self.valid_nodes[0].parent.identifier == self.root.identifier:
-            self.valid_nodes = []
+        elif len(self.valid_nodes_for_undo) == 1:
+            self.valid_nodes_for_undo = []
             return []
+
         else:
-            unique_parents = {node.parent for node in self.valid_nodes}
-            self.valid_nodes = list(unique_parents)
+            self.valid_nodes_for_undo.pop()
+            prev_valid_nodes = self.valid_nodes_for_undo[-1]
+
             return get_autocomplete_candidates(
-                self.valid_nodes, max_candidates, self.alpha
+                prev_valid_nodes, max_candidates, self.alpha
             )
